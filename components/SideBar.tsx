dab22ee536c80f6da6c8df9ddb2362c03d8f1e64
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-06-08 22:10:36 +0530

    user image show and fix the next.config.mjs appDir issue

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import NewChat from "./NewChat";
+import Image from "next/image";
 
 function SideBar() {
+  const { data: session } = useSession();
+  console.log(session?.user?.image!);
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -11,6 +17,15 @@ function SideBar() {
         <div>{/* ModelSelection*/}</div>
         {/* Map through three CharTows */}
       </div>
+      {session && (
+        <Image
+          src={session?.user?.image!}
+          alt="Profile pic"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+          width={100}
+          height={500}
+        />
+      )}
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,18 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['links.papareact.com']
-    },
-    experimental: {
-        appDir: true,
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'links.papareact.com',
+              pathname: '/**',
+            },
+            {
+              protocol: 'https',
+              hostname: 'lh3.googleusercontent.com',
+              pathname: '/**',
+            },
+        ]
     },
 };
 
