58fe7284ef0b76db94447c871d24e3937b15c715
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-06-08 16:27:48 +0530

    simple sidebar component done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SideBar from "@/components/SideBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="flex">
-          {/* Sidebar */}
+          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+            <SideBar />
+          </div>
           {/* ClientProvider - Notification */}
           <div className="bg-[#343541] flex-1">{children}</div>
         </div>
