27ae8be2b04918379458d0d2d044b901ead1ac1f
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-06-09 14:59:51 +0530

    delete the chat functionality worked

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,7 +1,7 @@
 import { db } from "@/firebase";
 import { ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
 import { TrashIcon } from "@heroicons/react/24/solid";
-import { collection, orderBy, query } from "firebase/firestore";
+import { collection, deleteDoc, doc, orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
@@ -34,6 +34,11 @@ const ChatRow = ({ id }: Props) => {
     setActive(pathname.includes(id));
   }, [pathname]);
 
+  const removeChat = async () => {
+    await deleteDoc(doc(db, "users", session?.user?.email!, "chats", id));
+    router.replace("/");
+  };
+
   return (
     <Link
       href={`/chat/${id}`}
@@ -43,7 +48,10 @@ const ChatRow = ({ id }: Props) => {
       <p className="flex-1 hidden md:inline-flex truncate">
         {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
       </p>
-      <TrashIcon className="h-5 w-5 text-gray-700 hover:text-red-700" />
+      <TrashIcon
+        onClick={removeChat}
+        className="h-5 w-5 text-gray-700 hover:text-red-700"
+      />
     </Link>
   );
 };
